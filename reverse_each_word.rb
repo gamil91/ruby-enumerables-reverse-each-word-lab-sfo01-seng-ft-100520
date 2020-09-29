b90323c635ca316234eb8bdd0eae5ff709b93ff9
Alexa Gamil <alexis_gamil99@yahoo.com>
2020-09-29 19:09:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,9 @@
-require 'pry'
-
 def reverse_each_word (string)
-  string_array = string.split(" ")
-  new_array = string_array.collect do |word| 
+  new_array = string.split.collect do |word| 
     word.reverse
   end
-  new_array.join(" ")
-end
+ new_array.join(" ")
+end 
+
+
 
